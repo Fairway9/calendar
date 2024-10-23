b0a85c34a0580a8edcfa770213356fc5c2b67b86
Fairway9 <digitalmarketingfairway@gmail.com>
2024-10-23 16:36:24 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,8 +4,4 @@ const nextConfig = {
   // Add any other configuration options here
 };
 
-<<<<<<< HEAD
 module.exports = nextConfig;
-=======
-module.exports = nextConfig;
->>>>>>> 6390f5a0a3e92f6ac94566f3fd1b6275d55b7f96
